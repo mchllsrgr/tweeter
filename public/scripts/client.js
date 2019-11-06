42ee9ab63302c4af82e44e6b4f1e259882ea90f7
Michelle Siregar <michellesiregar@gmail.com>
2019-11-06 21:46:28 +0000

    add ajax request to fetch tweet json and render html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -25,20 +25,26 @@ const renderTweets = function(tweets) {
     let $tweet = createTweetElement(tweet);
     $('#feed').prepend($tweet);
   }
-}
+};
 
 // fetch tweets from /tweets page
 const loadTweets = function() {
-  $.ajax('/tweets');
+  $.ajax({
+    url: '/tweets',
+    success: function(data) {
+      renderTweets(data);
+    }
+  });
 };
 
+
 $(document).ready(function() {
-  renderTweets(data);
+  loadTweets();
 
   $(function() {
     $('.new-tweet form').submit(function(event) {
       event.preventDefault();
-      $.ajax({ 
+      $.ajax({
         url: '/tweets',
         method: 'POST',
         data: $(this).serialize()
@@ -47,33 +53,3 @@ $(document).ready(function() {
   });
 
 });
-
-
-
-
-
-// TEST CODE
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
