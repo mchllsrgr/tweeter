50d80b2a89faee42f9300dcf7aab69f473276374
Michelle Siregar <michellesiregar@gmail.com>
2019-11-06 02:04:14 +0000

    added jquery plugin to convert timestamp to time ago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,13 +1,15 @@
-const createTweetElement = function(obj) {
-  const date = new Date(obj.created_at * 1000);
+// HELPER FUNCTIONS
+// put individual tweet in article element
+const createTweetElement = function(tweet) {
+  const date = jQuery.timeago(new Date(tweet.created_at * 1000));
   const markup = `
   <article class="tweet">
     <header>
-      <img src="${obj.user.avatars}">
-      <h3>${obj.user.name}</h3>
-      <a class="handle">${obj.user.handle}</a>
+      <img src="${tweet.user.avatars}">
+      <h3>${tweet.user.name}</h3>
+      <a class="handle">${tweet.user.handle}</a>
     </header>
-    <p class="tweet-text">${obj.content.text}</p>
+    <p class="tweet-text">${tweet.content.text}</p>
     <footer>
       <p>${date}</p>
       <p class="icons">Icons</p>
@@ -17,23 +19,44 @@ const createTweetElement = function(obj) {
   return markup;
 };
 
+// loop through array to render tweets
+const renderTweets = function(tweets) {
+  for (let tweet of tweets) {
+    let $tweet = createTweetElement(tweet);
+    $('#feed').prepend($tweet);
+  }
+}
+
 $(document).ready(function() {
-  const $tweet = createTweetElement(tweetData);
-  $('#feed').prepend($tweet);
+  renderTweets(data);
 });
 
+
+
+
+
 // TEST CODE
-const tweetData = {
+const data = [
+  {
     "user": {
       "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
     "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
     "created_at": 1461116232227
- }
-
-// Test / driver code (temporary)
-console.log($tweet); 
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
