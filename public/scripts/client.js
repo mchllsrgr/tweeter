c839a1def117f40de553fc42365d0f100e04ae4e
Michelle Siregar <michellesiregar@gmail.com>
2019-11-06 01:16:32 +0000

    client js fn - convert tweet object to html article markup

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,40 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+const createTweetElement = function(obj) {
+  const date = new Date(obj.created_at * 1000);
+  const markup = `
+  <article class="tweet">
+    <header>
+      <img src="${obj.user.avatars}">
+      <h3>${obj.user.name}</h3>
+      <a class="handle">${obj.user.handle}</a>
+    </header>
+    <p class="tweet-text">${obj.content.text}</p>
+    <footer>
+      <p>${date}</p>
+      <p class="icons">Icons</p>
+    </footer>
+  </article>
+  `;
+  return markup;
+};
 
+$(document).ready(function() {
+  const newTweet = $('<article>').addClass('tweet');
+});
+
+// TEST CODE
+const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+ }
+
+const $tweet = createTweetElement(tweetData);
+
+// Test / driver code (temporary)
+console.log($tweet); 
